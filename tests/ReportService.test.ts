d861734e17f2f7783475815cac3bb6d7b94b54f0
Bohdan <bogmarin@tutamail.com>
2026-07-30 19:00:20 +0300

    Create ReportService.test.ts

---
+++ tests/ReportService.test.ts
@@ -0,0 +1,13 @@
+import { ReportService } from "../src/services/ReportService";
+
+describe("ReportService", () => {
+
+    test("service exists", () => {
+
+        const service = new ReportService();
+
+        expect(service).toBeDefined();
+
+    });
+
+});
